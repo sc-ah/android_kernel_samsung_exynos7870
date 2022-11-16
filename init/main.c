fd31d3e9f717781b31fa4db9df6a703d1398f9d9
j0sh1x <aljoshua.hell@gmail.com>
2022-11-16 21:52:32 +0100

    enable exynos kvm patch hopefully

--- init/main.c
+++ init/main.c
@@ -573,6 +573,10 @@ asmlinkage __visible void __init start_kernel(void)
 	sort_main_extable();
 	trap_init();
 	mm_init();
+#ifdef CONFIG_KVM
+    void preinit_hyp_mode(void);
+    preinit_hyp_mode();
+#endif
 
 	/*
 	 * Set up the scheduler prior starting any interrupts (such as the
